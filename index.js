85762f329c6c3c4506218842955841c8dc295fab
freshjspd <freshjs.pd@gmail.com>
2023-06-27 13:35:40 +0300

    example with os  module

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const fs = require("fs");
+const os = require("os");
+const util = require("util");
+const path = require("path");
 
-const data = fs.readFileSync('./readme.md', {
-    encoding: 'utf8'
-});
+const userHostName = os.hostname();
+const userHomeDir = os.homedir();
+const userCore = os.cpus();
+const userInfo = os.userInfo();
 
-console.log(data);
+console.log(userInfo);
+console.log(userHostName);
+console.log(userHomeDir);
+console.log(userCore);
+
