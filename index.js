00faad1d48aa6c3674647d5672fe7607bbf15ccb
freshjspd <freshjs.pd@gmail.com>
2023-06-27 17:02:45 +0300

    hash for password. bcrypt

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http = require('http');
+const bcrypt = require('bcrypt');
 const requestListener = require('./requestListener.js');
 const PORT = 3000;
+
+const password = 'admin';
+const passwordHash = bcrypt.hashSync(password, 10);
+console.log('password hash is ', passwordHash);
 
 const server = http.createServer(requestListener);
 server.listen(PORT);
