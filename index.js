c72b3f12cfa627df7d2ccf59b00f2e5258559bc7
freshjspd <freshjs.pd@gmail.com>
2023-06-27 17:17:14 +0300

    send page for client

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const http = require('http');
-const bcrypt = require('bcrypt');
 const requestListener = require('./requestListener.js');
 const PORT = 3000;
-
-const password = 'admin';
-const passwordHash = bcrypt.hashSync(password, 10);
-console.log('password hash is ', passwordHash);
+const HOST = '127.0.0.1' // 'localhost'
 
 const server = http.createServer(requestListener);
-server.listen(PORT);
+server.listen(PORT, HOST, () => {
+    console.log(`Server is listening ${HOST} on ${PORT} port`);
+});
 
 
--- requestListener.js
+++ requestListener.js
@@ -3,5 +3,31 @@
 
 module.exports = function requestListener(req, res){
     const {url, method} = req;
-    res.end(`Hello, client! Yout method is ${method} in url ${url}`);
+    console.log('url = ', url);
+    console.log('method = ', method);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <h1>Home page</h1>
+    <p>About documentation
+    There are several types of documentation available on this website:
+    
+    API reference documentation
+    ES6 features
+    Guides
+    API reference documentation
+    The API reference documentation provides detailed information about a function or object in Node.js. This documentation indicates what arguments a method accepts, the return value of that method, and what errors may be related to that method. It also indicates which methods are available for different versions of Node.js.
+    
+    This documentation describes the built-in modules provided by Node.js. It does not document modules provided by the community.
+    
+    </p>    
+    </body>
+    </html>`);
 }
