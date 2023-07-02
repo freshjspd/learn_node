ab493019bf9e9d0ec14d4a321568f6b0a2aee1f3
freshjspd <freshjs.pd@gmail.com>
2023-07-02 10:09:27 +0300

    send page to client

--- requestListener.js
+++ requestListener.js
@@ -1,6 +1,12 @@
 // request - запрос. запрос от клиента, что он хочет
 // response - ответ. отклик сервера на запрос клиента
 
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+//const readFile = util.promisify(fs.readFile);
+
 module.exports = 
 function requestListener(req, res){
     const {url, method} = req;
@@ -8,16 +14,8 @@ function requestListener(req, res){
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Test page</title>
-    </head>
-    <body>
-        <h1>Lorem ipsum dolor sit amet.</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam maiores eos, dolor tempora alias dolore, pariatur illum culpa explicabo earum porro minima deserunt suscipit aspernatur laudantium. Quo, nemo quibusdam. Quisquam laborum, nihil ipsa ad fugiat eveniet in dolorem recusandae animi porro, totam quibusdam distinctio omnis et iure? Earum assumenda maiores, dolore vitae, esse itaque delectus aliquid tenetur commodi architecto in cumque dignissimos. Itaque alias eveniet deleniti nobis dolores accusantium, quisquam obcaecati molestias autem aperiam dicta tempora? Exercitationem harum vitae minima numquam a inventore illo mollitia iure alias provident quas tempore, dolore culpa quia! Alias ipsam laborum dolorum consectetur et nesciunt.</p>
-    </body>
-    </html>`);
+    const file = fs.readFileSync('./pages/about.html', {
+        encoding: 'utf8',
+    });
+    res.end(file);
 }
