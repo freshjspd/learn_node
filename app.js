8507cb36b85d9cdfb5730215b66362b71ddad694
freshjspd <freshjs.pd@gmail.com>
2023-07-03 12:28:23 +0300

    CRUD users

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+
 app.use(express.json());
 app.use('/', express.static("public"));
 
-app.use('*', (req, res) => {
-    res.status(404).sendFile(path.resolve(__dirname, '404.html'));
-})
+//CRUD for user
+// controller for user
 
+// get all users
+app.get("/users", (req, res) => {});
+// get user by id
+app.get("users/id", (req, res) => {});
+// create new user
+app.post("/users", (req,res) => {});
+// update user by id
+app.patch("/users/id", (req, res) => {});
+// delete user by id
+app.delete("/users/id", (req, res) => {});
 
 module.exports = app;
