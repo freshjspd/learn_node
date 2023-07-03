1c15cdfbf8609d99eab48e9925a2bbdabd3d4a1e
freshjspd <freshjs.pd@gmail.com>
2023-07-03 14:00:20 +0300

    fix user controller: error found user

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,24 +10,44 @@ module.exports.getAllUsers = (req, res) => {
 module.exports.getUser = (req, res) => {
       const {id} = req.params;
       const foundUser = Users.getUserById(id);
-      res.status(200).send(foundUser);
+      if(foundUser){
+            res.status(200).send(foundUser);
+            return;
+      } else{
+            res.status(404).send(`User # ${id} not found in database.`)
+      }
 };
 
 module.exports.createUser = (req, res) => {
       const {body} = req;
       const newUser = Users.createUser(body);
-      res.status(201).send(newUser);
+      if(newUser){
+            res.status(201).send(newUser);
+            return;
+      } else{
+            res.status(400).send('Bad request. New user is not created')
+      }
 };
 
 module.exports.updateUser = (req, res) => {
       const {id} = req.params;
       const {body} = req;
       const foundUser = Users.updateUser(id, body);
-      res.status(200).send(foundUser);
+      if(foundUser){
+            res.status(200).send(foundUser);
+            return;
+      } else{
+            res.status(404).send(`User # ${id} not found in database.`)
+      }
 };
   
 module.exports.deleteUser = (req, res) => {
     const {id} = req.params;
     const foundUser = Users.deleteUser(id);
-    res.status(200).send(foundUser);
+    if(foundUser){
+      res.status(200).send(foundUser);
+      return;
+} else{
+      res.status(404).send(`User # ${id} not found in database.`)
+}
 };
