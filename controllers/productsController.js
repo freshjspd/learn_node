29d54f02c5ea94c03e37514e3d9077d63e27711b
freshjspd <freshjs.pd@gmail.com>
2023-07-08 11:00:13 +0300

    practice: product controller methods were added

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,9 +1,38 @@
 const {Product} = require('./../models');
 
 module.exports.getProducts = (req, res) => {
-
+    const data = Product.getAllProducts();
+    res.status(200).send(data);
 }
-module.exports.getProduct = (req, res) => {}
-module.exports.createNewProduct = (req, res) => {}
-module.exports.updateProduct = (req, res) => {}
-module.exports.deleteProduct = (req, res) => {}
+
+module.exports.getProduct = (req, res) => {
+    const foundProduct = Product.getProductById(req.params.id);
+    if(foundProduct){
+        res.status(201).send(foundProduct);
+        return;
+    }
+    res.status(404).send(`User # ${req.params.id} not found`);
+}
+
+module.exports.createNewProduct = (req, res) => {
+    const newProduct = Product.createProduct(req.body);
+    res.status(200).send(newProduct);
+}
+
+module.exports.updateProduct = (req, res) => {
+    const foundProduct = Product.updateProductById(req.params.id, req.body);
+    if(foundProduct){
+        res.status(201).send(foundProduct);
+        return;
+    }
+    res.status(404).send(`User # ${req.params.id} not found`);
+}
+
+module.exports.deleteProduct = (req, res) => {
+    const foundProduct = Product.deleteProductById(req.params.id);
+    if(foundProduct){
+        res.status(201).send(foundProduct);
+        return;
+    }
+    res.status(404).send(`User # ${req.params.id} not found`);
+}
